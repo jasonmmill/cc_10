1f9a2842b9bdade606097b455f57e8c9eda601e4
Jasonmmill <jasonmatthewmill@gmail.com>
2025-02-23 22:12:46 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -18,3 +18,21 @@ const prod1 = new Product("Laptop", 101, 1200, 10)
 console.log(prod1.getDetails())
 prod1.updateStock(3)
 console.log(prod1.getDetails())
+
+// Task 2 - Creating an Order Class
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId
+        this.product = product
+        this.quantity = quantity
+        this.product.updateStock(quantity)
+    }
+    getOrderDetails() {
+        const totalPrice = this.product.price * this.quantity
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${totalPrice}`
+    }
+}
+
+const order1 = new Order(501, prod1, 2)
+console.log(order1.getOrderDetails())
+console.log(prod1.getDetails())
